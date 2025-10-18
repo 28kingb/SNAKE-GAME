79dcaa620a027a70168aeb76d41b1c5204a0802b
Mohamed Fawme Ahamad Bilal Ahmad <93920293+28kingb@users.noreply.github.com>
2025-10-18 14:27:16 +0200

    snake

---
+++ snake/My Project/AssemblyInfo.vb
@@ -0,0 +1,22 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+
+
+<Assembly: AssemblyTitle("snake")> 
+<Assembly: AssemblyDescription("")> 
+<Assembly: AssemblyCompany("Home")> 
+<Assembly: AssemblyProduct("snake")> 
+<Assembly: AssemblyCopyright("Copyright © Home 2012")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+
+<Assembly: Guid("e04a6087-833c-4cd2-bc92-d871d59968ed")>
+
+
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
